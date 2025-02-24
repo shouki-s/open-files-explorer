e590845ad22e5e525181692d4ec4478e11395206
Shouki SAKAMOTO <32442796+shouki-s@users.noreply.github.com>
2025-02-24 17:40:25 +0900

    Remove Hello World command and related code
    Clean up unnecessary boilerplate code from initial extension setup, including:
    - Removing 'Hello World' command from package.json
    - Deleting console log and command registration in extension.ts
    - Simplifying context.subscriptions to remove unused disposable

--- src/extension.ts
+++ src/extension.ts
@@ -7,20 +7,6 @@ import { OpenEditorItem } from './openEditorItem';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "structured-open-editors" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('structured-open-editors.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from structured-open-editors!');
-	});
-
 	// ツリービューの登録
 	const treeDataProvider = new OpenEditorsTreeProvider();
 	const treeView = vscode.window.createTreeView('structuredOpenEditors', {
@@ -49,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(disposable, treeView, closeFileCommand);
+	context.subscriptions.push(treeView, closeFileCommand);
 }
 
 // This method is called when your extension is deactivated
